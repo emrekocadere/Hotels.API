534cac82d230291a756548c2b31e97d9d70f11bb
emrekocadere <emrekocadere34@gmail.com>
2025-08-27 21:16:02 +0300

    feat: add Price property to Room entity with migration

--- Hotels.Domain/Entities/Room.cs
+++ Hotels.Domain/Entities/Room.cs
@@ -4,6 +4,7 @@ public class Room
 {
     public int Id { get; set; }
     public required string Name { get; set; }
+    public double Price { get; set; }
     public int HotelId { get; set; }
     
     public Hotel? Hotel { get; set; }
---
+++ Hotels.Infrastructure/Migrations/20250827181509_AddPriceColumnToRoomTable.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Hotels.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPriceColumnToRoomTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "Price",
+                table: "Rooms",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Price",
+                table: "Rooms");
+        }
+    }
+}
