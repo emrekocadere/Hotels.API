599ecade42f470c3a8f4764427f58bc6996342dc
emrekocadere <emrekocadere34@gmail.com>
2025-09-20 00:55:13 +0300

    refactor: update CreateHotelCommand properties and handler

--- Hotels.Application/Hotels/Commands/CreateHotel/CreateHotelCommand.cs
+++ Hotels.Application/Hotels/Commands/CreateHotel/CreateHotelCommand.cs
@@ -1,16 +1,19 @@
+using Hotels.Domain.Entities;
 using MediatR;
 
 namespace Hotels.Application.Hotels.Commands.CreateHotel;
 
 public class CreateHotelCommand : IRequest<int>
 {
-    public required string Name { get; set; }
-    public required short Star { get; set; }
-    public required string ContactEmail { get; set; }
-    public required string ContactNumber { get; set; }
-    public required string City { get; set; }
-    public required string Country { get; set; }
-    public required string PostalCode { get; set; }
+    public  string Name { get; set; }
+    public  short Star { get; set; }
+    public  string ContactEmail { get; set; }
+    public  string ContactNumber { get; set; }
+    public  int CityId { get; set; }
+    public TimeOnly CheckInTime { get; set; }
+    public TimeOnly CheckOutTime { get; set; }
+    public ICollection<int>? FeatureIds { get; set; }
+
 }
 
 
--- Hotels.Application/Hotels/Commands/CreateHotel/CreateHotelCommandHandler.cs
+++ Hotels.Application/Hotels/Commands/CreateHotel/CreateHotelCommandHandler.cs
@@ -10,8 +10,8 @@ public class CreateHotelCommandHandler(IMapper mapper, IRepository<Hotel> hotels
 {
     public async Task<int> Handle(CreateHotelCommand request, CancellationToken cancellationToken)
     {
-        var hotel = mapper.Map<Hotel>(request);
-       await hotelsRepository.Create(hotel);
+        var hotel = mapper.Map<Hotel>(request); 
+        await hotelsRepository.Create(hotel);
         var affectedRows= await hotelsRepository.SaveChanges();
         if (affectedRows > 0)
             return hotel.Id;
